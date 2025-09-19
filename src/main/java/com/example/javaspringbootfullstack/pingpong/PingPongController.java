006655234286541187e7bee11915ef0ed3ff4171
Abdulrahman <abdulrahman.alzn@gmail.com>
2025-09-19 18:25:14 +0300

    add ping pong

---
+++ src/main/java/com/example/javaspringbootfullstack/pingpong/PingPongController.java
@@ -0,0 +1,15 @@
+package com.example.javaspringbootfullstack.pingpong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/ping")
+public class PingPongController {
+
+    @GetMapping
+    public String pingPong(){
+        return "Pong";
+    }
+}
