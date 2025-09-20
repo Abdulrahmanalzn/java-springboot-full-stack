c674cfacd364690fb87a4317e4a74d95a9f3ca82
Abdulrahman <abdulrahman.alzn@gmail.com>
2025-09-20 22:13:21 +0300

    add some details

--- src/main/java/com/example/javaspringbootfullstack/pingpong/PingPongController.java
+++ src/main/java/com/example/javaspringbootfullstack/pingpong/PingPongController.java
@@ -10,6 +10,8 @@ public class PingPongController {
 
     @GetMapping(produces = "text/html")  // tell Spring to return HTML
     public String pingPong(){
-        return "<h1 style='color:red; font-size:40px;'>❤ I love u dhauooooo ❤ </h1>";
+        return "<h1 style='color:red; font-size:40px;'>" +
+                "The wonderful, the only queen, the one, the perfect, the gorgeous, the radiant, the divine, the graceful, the flawless, the enchanting, the majestic, the elegant, the breathtaking, the angelic, the dazzling, the supreme, the unforgettable, the unmatched, the rare jewel, the true goddess. DHAY " +
+                " ❤ I love u dhauooooo ❤ </h1>";
     }
 }
